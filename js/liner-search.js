9829475e36f07ee309b19e29d38bd1870e984664
mahsa <mahsaa.rajabpour@gmail.com>
2024-06-12 00:04:10 +0330

    add(liner search)

--- js/liner-search.js
+++ js/liner-search.js
@@ -1,5 +1,15 @@
 // ::::::linear Search::::::
 
 const linerSearch =  (array,elementToFind) => {
+ let result;
+    for(let i=0;i<array.length;i++){
+        if(array[i]===elementToFind){
+           return "the element index is: " + i
+        }
+    }
+    return "The element is not in this list."
+};
 
-}
+let linerSearchArray=[2,5,1,4,7,3];
+let elementToFind=7;
+console.log(linerSearch(linerSearchArray,elementToFind));
